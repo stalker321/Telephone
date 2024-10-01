6f120fc0479998636a90c6728e88d4fb9fe14883
Konstantin <ns20102076@mail.ru>
2024-10-01 20:42:18 +0300

    End

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,33 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    display = ui->lineEdit;
+    call = ui->pushButtonCALL;
+
+    wrongNumber();
+    QObject::connect(display, &QLineEdit::textChanged, [this](QString text){
+        QRegularExpression mobileNumber("^\\+\\d{11}$");
+        QRegularExpression homeNumber("^\\d{6}$");
+        if (mobileNumber.match(text).hasMatch()) correctNumber();
+        else if (homeNumber.match(text).hasMatch()) correctNumber();
+        else wrongNumber();
+    });
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::correctNumber()
+{
+    call->setStyleSheet("QPushButton {background-color: green}");
+    call->setEnabled(true);
+}
+
+void MainWindow::wrongNumber()
+{
+    call->setStyleSheet("QPushButton {background-color: red}");
+    call->setEnabled(false);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QRegularExpression>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QPalette>
+#include <QObject>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,7 +18,34 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
+    QLineEdit* display;
+    QPushButton* call;
     ~MainWindow();
+
+public slots:
+    void add0(){display->setText(display->text() + "0");};
+    void add1(){display->setText(display->text() + "1");};
+    void add2(){display->setText(display->text() + "2");};
+    void add3(){display->setText(display->text() + "3");};
+    void add4(){display->setText(display->text() + "4");};
+    void add5(){display->setText(display->text() + "5");};
+    void add6(){display->setText(display->text() + "6");};
+    void add7(){display->setText(display->text() + "7");};
+    void add8(){display->setText(display->text() + "8");};
+    void add9(){display->setText(display->text() + "9");};
+    void addP(){display->setText(display->text() + "+");};
+    void remove(){
+        if (display->text().size() < 1) {
+            return;
+        } else {
+            display->setText(display->text().remove(display->text().size() -1, 1));
+        }
+    };
+    void clear(){display->setText("");};
+
+private slots:
+    void correctNumber();
+    void wrongNumber();
 
 private:
     Ui::MainWindow *ui;
